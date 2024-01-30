ce0988d54edbb46745d8cb30829fd705ff61f20d
Rohit Chouhan <Rohitchouhan201090@gmail.com>
2024-01-30 22:48:43 +0530

    Create Routes and Controller

---
+++ Server/Routes/user.Routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { test } from "../Controllers/user.Controller.js";
+
+const router=express.Router();
+
+router.get("/test",test)
+
+
+export default router;
--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,21 @@
 import express from  "express";
 import dotenv from 'dotenv';
 dotenv.config();
-
+import userRoutes from "./Routes/user.Routes.js"
 const app=express();
 
 // Db Connected
 
 import { connectDatabase } from "./Models/config.js";
 connectDatabase();
+
+// logger
+ import logger from "morgan"
+ app.use(logger("tiny"))
+
+//  Routes
+
+ app.use('/User',userRoutes)
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server Are running On Port ${process.env.PORT}`)
