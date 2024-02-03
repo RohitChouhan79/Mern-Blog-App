be27b269ae6380547065a38941a323a3ce8e8e85
Rohit Chouhan <Rohitchouhan201090@gmail.com>
2024-02-03 22:03:28 +0530

    darkmode

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { Avatar, Button, Dropdown, Navbar, NavbarToggle, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link, NavLink,useLocation} from 'react-router-dom'
-import { FaMoon, FaSearch } from 'react-icons/fa';
-import {useSelector} from "react-redux"
+import { FaMoon, FaSearch,FaSun} from 'react-icons/fa';
+import {useSelector,useDispatch} from "react-redux"
+import { toggleTheme } from '../redux/theme/themeSlice';
 export const Header = () => {
   const {currentUser}=useSelector(state=>state.user)
-  const path=useLocation().pathname;
+  const {theme}=useSelector(state=>state.theme)
+  const dispatch=useDispatch()
+  // const path=useLocation().pathname;
   return (
     <Navbar className=' border-b-2'>
       <Link to='/' className=' self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -19,8 +22,9 @@ export const Header = () => {
         <FaSearch/>
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className=' w-12 h-10 hidden sm:inline ' color='gray' pill>
-          <FaMoon/>
+      <Button className=' w-12 h-10 hidden sm:inline ' onClick={()=>dispatch(toggleTheme())} color='gray' pill>
+        {theme==='light' ? <FaSun />:<FaMoon/>}
+          
         </Button>
         {currentUser ? (
           <Dropdown arrowIcon={false} inline label={
---
+++ client/src/components/ThemeProvider.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export default function ThemeProvider({children}) {
+    const {theme}=useSelector((state)=>state.theme)
+  return (
+
+    <div className={theme}>
+        <div className='bg-white text-black dark:text-white dark:bg-black min-h-screen'>
+            {children}
+        </div>
+    </div>
+  )
+}
