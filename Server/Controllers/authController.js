a3cab5d1c17058a914adb6e2f43aec95042f86d6
Rohit Chouhan <Rohitchouhan201090@gmail.com>
2024-02-13 17:31:35 +0530

    add Admin

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -21,7 +21,7 @@ export const signup= async (req,res,next)=>{
 
     try {
         await newUser.save();
-        const token=jwt.sign({id:newUser._id},process.env.JWT_SECRET_KEY);
+        const token=jwt.sign({id:newUser._id,isAdmin:newUser.isAdmin},process.env.JWT_SECRET_KEY);
         const {password: pass,...rest}=newUser._doc
         res.status(200).cookie('access_token',token,{
             httpOnly:true,}).json({message:'Signup Succesfull',id:newUser._id,sucess:true}) 
@@ -48,7 +48,7 @@ export const signin= async (req,res,next)=>{
         if(!validPassword){
             return next(new Errorhandler("Invalid Password",400))
         }
-        const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET_KEY);
+        const token=jwt.sign({id:validUser._id,isAdmin: validUser.isAdmin},process.env.JWT_SECRET_KEY);
         const {password: pass,...rest}=validUser._doc
         res.status(200).cookie('access_token',token,{
             httpOnly:true,}).json(rest)
@@ -64,7 +64,7 @@ export const google= async (req,res,next)=>{
         const user= await User.findOne({email})
         // console.log(user)
         if(user){
-            const token=jwt.sign({id:user._id},process.env.JWT_SECRET_KEY);
+            const token=jwt.sign({id:user._id,isAdmin:user.isAdmin},process.env.JWT_SECRET_KEY);
             const {password: pass,...rest}=user._doc
             res.status(200).cookie('access_token',token,{
             httpOnly:true,}).json(rest)
@@ -80,7 +80,7 @@ export const google= async (req,res,next)=>{
 
             });
             await newuser.save();
-            const token=jwt.sign({id:newuser._id},process.env.JWT_SECRET_KEY);
+            const token=jwt.sign({id:newuser._id,isAdmin:user.isAdmin},process.env.JWT_SECRET_KEY);
             const {password: pass,...rest}=newuser._doc
             res.status(200).cookie('access_token',token,{
             httpOnly:true,}).json(rest)
